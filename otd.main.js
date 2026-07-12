5acea6fb3747e0645a70b9d38ff01ad3c6e3c170
PLANETSRV <mondaiajah1w@gmail.com>
2026-07-13 02:38:28 +0700

    Update otd.main.js

--- otd.main.js
+++ otd.main.js
@@ -1,1 +1,6 @@
-document.getElementById('otd').innerHTML = '<span>Desain by :<a href="layanan.html" class="mx-2">PLANNET</a></span>';
+document.addEventListener('DOMContentLoaded', function () {
+      const target = document.getElementById('otd');
+      if (target) {
+        target.innerHTML = '<span>Desain by : <a href="layanan.html" class="mx-2">PLANNET</a></span>';
+      }
+    });
